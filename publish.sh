aac918c8eaaad78457ac880659950833877afcfa
sal <slpg500a>
2018-03-30 10:58:22 +0100

    Make it so that it pushes pages

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+if [ -n "$GITHUB_API_KEY" ]; then
+    cd out
+    # This generates a `web` directory containing the website.
+    cd out
+    git init
+    git checkout -b gh-pages
+    git add .
+    git -c user.name='salimfadhley' -c user.email='salimfadhley@gmail.com' commit -m init
+    git push -f -q https://salimfadhley:$GITHUB_API_KEY@github.com/salimfadhley/cv gh-pages &2>/dev/null
+fi
