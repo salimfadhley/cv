5a224a3ac10120208ab2a83aee3d28203fb86df5
sal <slpg500a>
2018-03-30 12:51:44 +0100

    Add some styling

--- publish.sh
+++ publish.sh
@@ -8,6 +8,7 @@ if [ -n "$GITHUB_API_KEY" ]; then
     git add .
     git -c user.name='salimfadhley' -c user.email='salimfadhley@gmail.com' commit -m init
     git push -f -q https://salimfadhley:$GITHUB_API_KEY@github.com/salimfadhley/cv.git gh-pages &2>/dev/null
+    echo "Pushed to GitHub."
 else
     echo "GITHUB_API_KEY not set"
 fi
