6557301a9670a2f5069e510ca8aa4fd3fe880f92
sal <slpg500a>
2019-02-01 20:56:28 +0000

    Use regular docker, not compose

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-mkdir -p /project/out
-docker-compose run jinja2cli cv.tex.j2 confidential.yaml > ./tex/cv_confidential.tex
-docker-compose run jinja2cli cv.tex.j2 public.yaml > ./tex/cv.tex
-docker-compose run xelatex -output-directory=/project/out cv_confidential.tex
-docker-compose run xelatex -output-directory=/project/out cv.tex
+mkdir -p ./project/out
+docker run -v `pwd`:/project -w /project/tex vikingco/jinja2cli:latest cv.tex.j2 confidential.yaml > ./tex/cv_confidential.tex
+docker run -v `pwd`:/project -w /project/tex vikingco/jinja2cli:latest cv.tex.j2 public.yaml > ./tex/cv.tex
+docker run -v `pwd`:/project -w /project/tex moss/xelatex xelatex -output-directory=/project/out cv_confidential.tex
+docker run -v `pwd`:/project -w /project/tex moss/xelatex xelatex -output-directory=/project/out cv.tex
