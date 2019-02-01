8b8457d8a4b5e62ab521d503c2eee124662d22fb
sal <slpg500a>
2019-02-01 17:19:06 +0000

    Merge branch 'master' of github.com:salimfadhley/cv
    # Conflicts:
    #	cv.tex

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mkdir -p /project/out
 docker-compose run jinja2cli cv.tex.j2 confidential.yaml > ./tex/cv_confidential.tex
-docker-compose run jinja2cli cv.tex.j2 public.yaml > ./tex/cv_public.tex
+docker-compose run jinja2cli cv.tex.j2 public.yaml > ./tex/cv.tex
 docker-compose run xelatex -output-directory=/project/out cv_confidential.tex
-docker-compose run xelatex -output-directory=/project/out cv_public.tex
+docker-compose run xelatex -output-directory=/project/out cv.tex
